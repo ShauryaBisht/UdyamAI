122a09456b82d74c00a7a5f74593cb9659f82a3f
Shaurya Bisht <shauryabisht707@gmail.com>
2026-08-31 13:03:16 +0530

    bugs fixes

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -14,7 +14,7 @@ import app.models  # noqa: F401
 from app.config import settings
 
 config = context.config
-if config.config_file_name and config.file_config.has_section("loggers"):
+if config.config_file_name:
     fileConfig(config.config_file_name)
 
 target_metadata = SQLModel.metadata
